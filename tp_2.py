6e31b14de253ca056e76dcff4da55d9cd0850d09
Richardnegro03 <139642560+Richardnegro03@users.noreply.github.com>
2023-10-13 10:49:10 -0300

    Add files via upload

---
+++ tp_2.py
@@ -0,0 +1,48 @@
+#  Actividad:
+
+#1. Crear un proyecto “Calculadora” siguiendo todo el workflow explicado
+#2. Hacer cambios en el proyecto y commitearlos (mínimo 3/4 commits distintos)
+#3. Crear una rama que se llame: “nombre_usuario/testing_branches”
+#4. Hacer cambios en la rama y luego mergear los cambios a main/master
+#5. Compartir link del repositorio con la clase
+
+
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+print("Select operation.\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Exit")
+
+while True:
+    choice = input("Enter choice(1/2/3/4/5): ")
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        break
+    elif choice == '5':
+        print("Exit")
+        break
+    else:
+        print("Invalid Input")
